9cbcfbea4e25c262f2556a69e0809ab8582fda00
Hanna <northernwind@tut.by>
2021-10-04 19:02:18 +0300

    feat: solve hanoi-towers

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+  // The formula for 3 pegs and n discs is 2^n
+  // turnsSpeed === speed in turns per hour
+  let turnsNumber = Math.pow(2, disksNumber) - 1;
+  let speedSeconds = Math.floor((turnsNumber / (turnsSpeed/3600)));
+  return {
+    turns: turnsNumber,
+    seconds: speedSeconds
+};
 }
+
